33cf457b4491b8598a125243d087b4c6d99ba954
Weckatron <weckacore@gmail.com>
2020-01-23 12:06:20 -0500

    Added a general trends page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'main/home'
 
-  get 'main/trending'
+  get 'main/trending', as: 'trend'
 
-  get 'main/recomended'
+  get 'main/recomended', as: 'rec'
 
   root to: 'main#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
