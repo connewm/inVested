e670c1cf1e8d02067c2d5d2ab9409b07567db140
jtmarshall96 <43729282+jtmarshall96@users.noreply.github.com>
2020-02-12 20:34:00 -0500

    commiting

--- React/vested/src/App.js
+++ React/vested/src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    "oranges", "apples", "candy"
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://www.100daysofrealfood.com/wp-content/uploads/2011/06/popcorn1-500x500.jpg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="list-container">
+      <ListDisplay items={items} handleClick={(item) => {
+        setItems(items.slice().filter((i) => i !== item));
+      }}/>
+      <InputText handleSubmit={(item) => {
+          setItems(items.concat(item));
+        }}
+      />
     </div>
-  );
+  )
+}
+
+const ListItem = (props) => (
+  <li onClick={()=> props.handleClick(props.name)}>{props.name}</li>
+)
+
+const ListDisplay = (props) => {
+  const items = props.items.map((item, i) => (
+    <ListItem
+      key={i}
+      name={item}
+      handleClick={props.handleClick}
+    />
+  ))
+  return (
+    <ul>
+      {items}
+    </ul>
+  )
+}
+
+const InputText = (props) => {
+  const [value, setValue] = useState('')
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      props.handleSubmit(value);
+      setValue('');
+    }}>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
 }
 
 export default App;
