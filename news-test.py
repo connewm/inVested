30766823d1b88000f66e4e9bb122a148eeac71f2
connewm <newman.556@osu.edu>
2020-02-25 16:19:19 -0500

    update new api test

--- news-test.py
+++ news-test.py
@@ -3,6 +3,7 @@ import json
 
 newsapi = NewsApiClient(api_key='692ca85daa144ef3bda94bfe165d5aa5')
 
-all_articles = newsapi.get_everything(q='google', from_param='2020-02-25', to='2020-02-25', language='en', sort_by='relevancy')
-all_articles = json.dumps(all_articles)
-print(all_articles)
+all_articles = newsapi.get_everything(qintitle='+google', from_param='2020-02-25', to='2020-02-25', language='en')
+json_fmt_articles = json.dumps(all_articles)
+print(json_fmt_articles)
+print(all_articles['totalResults'])
