289e335eb0131ea888219e5a3bd9b0404e6072dd
shreyabyreddy <byreddy.4@osu.edu>
2020-03-03 17:58:52 -0500

    made a stock graph with dummy data

--- React/vested/src/Graph/Graph.js
+++ React/vested/src/Graph/Graph.js
@@ -1,12 +1,78 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // src/components/Content.js
+//npm install react-plotly.js plotly.js
 import React from 'react';
 import './Graph.css';
+import Plot from 'react-plotly.js';
 
 const Graph = (props) => {
   // incoming props to determine the graph to fetch
   return (
+  		<Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      />
       // TODO: replace static image with lamdba call to generate graph
-      <img src="https://stocksnips.net/wp-content/uploads/2018/05/stock-news-sentiment-vs-stock-price-2.png" className="fin-graph" alt="graph" />
+      //<img src="https://stocksnips.net/wp-content/uploads/2018/05/stock-news-sentiment-vs-stock-price-2.png" className="fin-graph" alt="graph" />
     );
 };
 
