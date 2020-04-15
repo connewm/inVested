004211416320a2b249db5968b60620eee829a73a
connewm <newman.556@osu.edu>
2020-04-15 14:26:27 -0400

    removed extraneous files

--- postgres_connect.py
+++
@@ -1,8 +0,0 @@
-
-def connect():
-    try:
-        connect = psycopg2.connect(database="postgres", user="postgres", password="CapStoneRDS", 
-        host="database-1.ciomlblnrvsp.us-east-2.rds.amazonaws.com", port='5432')
-        return connect
-    except:
-        print("no good")
